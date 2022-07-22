e60d5fd01897e8f0e73233cab34289a4a7ec9cb4
jlucartc <jlucartc@gmail.com>
2022-07-22 10:01:21 -0300

    Starting image cropping logic

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,6 +6,20 @@ class ApiController < ApplicationController
 
 	def download
 		binding.pry
+		file = Tempfile.create(binmode: true)
+		content = params["image_content"].gsub(/data:.+;base64,/,'')
+		decoded_content = Base64.decode64(content)
+		cropped_images = params["cropped_images"]
+		file.write(decoded_content)
+
+		source_image = Vips::Image.new_from_file(file.path)
+		
+		cropped_images.each do |image|
+			puts image
+		end
+
+		binding.pry
+
 		render json: {msg: 'ok'}, status: :ok
 	end
 
