5aededd0d94613f07886630f9fca5e20cb9af1a6
jlucartc <jlucartc@gmail.com>
2022-07-26 14:55:07 -0300

    Changing code to return only one zip file with all the cropped images

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,41 +5,50 @@ class ApiController < ApplicationController
 	end
 
 	def download
-		file = Tempfile.create(binmode: true)
 		output_images = []
-		content = params["image_content"].gsub(/data:.+;base64,/,'')
-		decoded_content = Base64.decode64(content)
+
+		sources = params["sources"]
 		cropped_images = params["cropped_images"]
-		file.write(decoded_content)
 
-		source_image = Vips::Image.new_from_file(file.path)
-		
-		cropped_images.each_with_index do |image,index|
-			cropped_image_width = image["image_width"]
-			cropped_image_heigth = image["image_height"]
-			cropped_image_distance_top = image["top"]
-			cropped_image_distance_left = image["left"]
-			container_width = image["container_width"]
-			container_heigth = image["container_height"]
-			scale = cropped_image_width.to_f/source_image.width.to_f
+		sources.map{|key,value|
+			file = new Tempfile.create(binmode: true)
+			file.write(Base64.decode64(value))
+			{key => file}
+		}
 
-			if scale > 1
-				source_image = source_image.resize(1 + ((cropped_image_width.to_f - source_image.width.to_f) / source_image.width.to_f))
-			else
-				source_image = source_image.resize(1 - ((source_image.width.to_f - cropped_image_width.to_f) / source_image.width.to_f))
+		sources.each do |source|{
+			source_image = Vips::Image.new_from_file(source.value.path)
+
+			cropped_images.each_with_index do |image,index|
+				if image["source"].to_s == source.key.to_s
+					cropped_image_width = image["image_width"].to_f
+					cropped_image_heigth = image["image_height"].to_f
+					cropped_image_distance_top = image["top"].to_f
+					cropped_image_distance_left = image["left"].to_f
+					crop_window_width = image["crop_windo_width"].to_f
+					crop_window_heigth = image["crop_window_height"].to_f
+					scale = cropped_image_width.to_f/source_image.width.to_f
+
+					if scale > 1
+						source_image = source_image.resize(1 + ((cropped_image_width - source_image.width) / source_image.width))
+					else
+						source_image = source_image.resize(1 - ((source_image.width - cropped_image_width) / source_image.width))
+					end
+
+					source_image = source_image.embed(
+														cropped_image_distance_left,
+														cropped_image_distance_top,
+														crop_window_width,
+														crop_window_heigth,
+														extend: :white
+													)
+
+					output = Tempfile.create(['','.jpeg'],binmode: true)
+					source_image.write_to_file(output.path)
+					output_images << output.path
+				end
 			end
-
-			source_image = source_image.embed(
-								cropped_image_distance_left,
-								cropped_image_distance_top,
-								container_width,
-								container_heigth,
-								extend: :white
-							)
-			output = Tempfile.create(['','.jpeg'],binmode: true)
-			source_image.write_to_file(output.path)
-			output_images << output.path
-		end
+		}
 
 		zipfile = Tempfile.create(binmode: true)
 
